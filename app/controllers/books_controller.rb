fad045ce5d4119bf6115a99154c00976a6e86b14
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-08-11 18:24:02 +0900

    successメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: 'You have creatad book successfully.'
     else
       @books = Book.all
       render :index
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: 'You have updated book successfully.'
     else
       render :edit
     end
