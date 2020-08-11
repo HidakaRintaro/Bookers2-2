79b3167e11b80168487c08bffda590260e6e036e
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-08-11 22:11:42 +0900

    favoritesのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books
+  has_many :favorites, dependent: :destroy
   attachment :profile_image
   validates :name, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
