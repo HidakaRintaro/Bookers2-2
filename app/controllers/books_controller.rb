9ac1f0bef06d66546336ea656d030158111e3ec4
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-08-11 11:56:19 +0900

    booksコントローラにcreateアクションを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,4 +9,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :show
+    end
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
