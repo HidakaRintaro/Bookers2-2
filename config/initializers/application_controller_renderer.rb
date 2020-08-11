3ca2d9ad460720ee16b28e18ce1d32e88a59f155
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-08-11 15:36:39 +0900

    usersコントローラにupdateアクションとストロングパラメータの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ac1915eb4ffb3c5953f14b6727a3a8b001f36e521ee1c0ca682da5404e642263476c11a4a6e6f263f582c33f571f172506ed7ea57a0dc97729638fb06be2493d'
